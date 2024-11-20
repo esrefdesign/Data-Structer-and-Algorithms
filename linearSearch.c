123381295e468ea2093fa606c8ef9956ea300eda
EsrefDesign <esrefdesign@gmail.com>
2024-11-21 01:10:23 +0300

    linearSearch algorithm

---
+++ linearSearch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linearSearch(int array[],int size,int wanted) { 
+    
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i]!=wanted)
+        {
+            continue;
+        }
+        else{
+            return i;
+        }
+    }
+    return -1;
+}
+
+void main(){
+    int size = 4;
+    int arr[4] = {2,6,32,15};
+    printf("this is the index of wanted number in array(if the index is '-1' array not to contain the number wanted ): %d\n",linearSearch(arr,size,15));
+    
+}
